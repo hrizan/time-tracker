5bb0f7d0ff68ba79b663d0eebd412be5e9c61285
tbmihailov@gmail.com <tbmihailov@gmail.com>
2014-10-18 16:21:27 +0300

    Added .Net data client for the TimeTracker api with Activity registration features

---
+++ src/TimeTracker/TimeTracker.RestDataClient.Tests/RestClientTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeTracker.RestDataClient.TimeTracker.ClientRest;
+using TimeTracker.Models;
+using TimeTracker.Backend.Models;
+using RestSharp;
+
+namespace TimeTracker.RestDataClient.Tests
+{
+    [TestClass]
+    public class RestClientTests
+    {
+        [TestMethod]
+        public void Test_Register_Activity()
+        {
+            string apiUrl = "";
+            string authKey = "";
+            Guid deviceId = new Guid();//trqbva da se zaredi ot nastroikite
+            string computerName = "DOBI_PC";
+            TimeTrackerDataService dataService = new TimeTrackerDataService(apiUrl, authKey);
+            
+            
+            string processName =  "Chrome.exe";
+            string resourceName =  "Facebook.com";
+            string resourceDescription = "Facebook - Momchil hardalov profile";//process title
+            DateTime processUsageFrom = DateTime.UtcNow.AddMinutes(-1);
+            DateTime processUsageTo  = DateTime.UtcNow;
+
+            var activityToRegister = new ActivityUpdateDto()
+            {
+                //device info
+                DeviceId = deviceId,
+                DeviceName = computerName,
+                DeviceTypeId = (int)DeviceType.Desktop,
+                OSTypeId = (int)OSType.Windows,
+
+                //process info
+                ProcessName = processName,
+                Resource = resourceName,
+                ResourceDescription = resourceDescription,
+                TimeFrom = processUsageFrom,
+                TimeTo = processUsageTo,
+                DurationInSec = (processUsageTo - processUsageFrom).TotalSeconds,
+            };
+
+            var res  = dataService.RegisterActivity(activityToRegister);
+
+            if (res.ResponseStatus == ResponseStatus.Completed 
+                && res.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                //Uspeshen e zapisa - trieme ot opashkata
+            }
+        }
+    }
+}
---
+++ src/TimeTracker/TimeTracker.RestDataClient/TimeTrackerDataService.cs
@@ -0,0 +1,130 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeTracker.Models;
+
+namespace TimeTracker.RestDataClient
+{
+
+    
+
+    namespace TimeTracker.ClientRest
+    {
+        public class TimeTrackerDataService
+        {
+            public const string AUTH_TOKEN = "AUTH_TOKEN";
+
+            string _AuthKey;
+
+            string _apiUrl;
+
+            public TimeTrackerDataService(string apiUrl)
+                : this(apiUrl, string.Empty)
+            { }
+
+
+            public TimeTrackerDataService(string apiUrl, string agentAuthKey)
+            {
+                _apiUrl = apiUrl;
+                _AuthKey = agentAuthKey;
+            }
+
+            private void AddCredentialsToRestRequest(RestRequest request)
+            {
+                if  (!string.IsNullOrEmpty(_AuthKey))
+                {
+                    request.AddHeader(AUTH_TOKEN, _AuthKey);
+                }
+            }
+
+            #region RegisterActivities
+            public void RegisterActivityAsync(ActivityUpdateDto activity, Action<IRestResponse<ActivityUpdateDto>> callback)
+            {
+                RestClient client = new RestClient(_apiUrl);
+                var request = PrepareRegisterActivityRequest(activity);
+
+                client.ExecuteAsync<ActivityUpdateDto>(request, callback);
+            }
+
+            public IRestResponse<ActivityUpdateDto> RegisterActivity(ActivityUpdateDto activity)
+            {
+                RestClient client = new RestClient(_apiUrl);
+                var request = PrepareRegisterActivityRequest(activity);
+
+                return client.Execute<ActivityUpdateDto>(request);
+            }
+
+            private RestRequest PrepareRegisterActivityRequest(ActivityUpdateDto activity)
+            {
+                var request = new RestRequest(new Uri("Activity/PostActivity", UriKind.Relative), Method.POST);
+
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(activity);
+                AddCredentialsToRestRequest(request);
+
+                return request;
+            }
+
+            #endregion
+
+            #region RegisterActivitiesList
+            public void RegisterModuleCheckListAsync(List<ActivityUpdateDto> activities, Action<IRestResponse<List<ActivityUpdateDto>>> callback)
+            {
+                RestClient client = new RestClient(_apiUrl);
+                var request = PrepareRegisterModuleCheckListRequest(activities);
+
+                client.ExecuteAsync<List<ActivityUpdateDto>>(request, callback);
+            }
+
+            public IRestResponse<List<ActivityUpdateDto>> RegisterActivityList(List<ActivityUpdateDto> activities)
+            {
+                RestClient client = new RestClient(_apiUrl);
+                var request = PrepareRegisterModuleCheckListRequest(activities);
+
+                return client.Execute<List<ActivityUpdateDto>>(request);
+            }
+
+            private RestRequest PrepareRegisterModuleCheckListRequest(List<ActivityUpdateDto> moduleChecks)
+            {
+                var request = new RestRequest(new Uri("Common/RegisterModuleCheckList", UriKind.Relative), Method.POST);
+
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(moduleChecks);
+                AddCredentialsToRestRequest(request);
+
+                return request;
+            }
+            #endregion
+
+            //#region GetSchedules
+            //public void GetSchedulesAsync(string agentGuid, Action<IRestResponse<List<Schedule>>> callback)
+            //{
+            //    RestClient client = new RestClient(_apiUrl);
+            //    var request = PrepareGetSchedulesRequest(agentGuid);
+
+            //    client.ExecuteAsync(request, callback);
+            //}
+
+            //public IRestResponse<List<Schedule>> GetSchedules(string agentGuid)
+            //{
+            //    RestClient client = new RestClient(_apiUrl);
+            //    var request = PrepareGetSchedulesRequest(agentGuid);
+
+            //    return client.Execute<List<Schedule>>(request);
+            //}
+
+            //private RestRequest PrepareGetSchedulesRequest(string agentGuid)
+            //{
+            //    string relativeUrl = string.Format("Common/GetSchedules?agentGuid={0}", agentGuid);
+            //    var request = new RestRequest(new Uri(relativeUrl, UriKind.Relative), Method.GET);
+            //    AddCredentialsToRestRequest(request);
+            //    return request;
+            //}
+            //#endregion
+
+        }
+    }
+
+}
