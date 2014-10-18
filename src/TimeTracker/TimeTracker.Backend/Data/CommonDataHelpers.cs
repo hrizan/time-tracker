7ef3d68c967b91b85c63d03dc40f10f8d572fb03
tbmihailov@gmail.com <tbmihailov@gmail.com>
2014-10-18 18:31:21 +0300

    Changed categorization filtering in activity registration

--- src/TimeTracker/TimeTracker.Backend/Data/CommonDataHelpers.cs
+++ src/TimeTracker/TimeTracker.Backend/Data/CommonDataHelpers.cs
@@ -9,10 +9,8 @@ namespace TimeTracker.Backend.Models
     {
         public static ActivityCategorization GetActivityCategorization(this TimeTrackerContext db, Guid consumerId, string processName, string resource)
         {
-            var categorization = db.ActivityCategorizations.Where(p => p.ConsumerId == consumerId && p.ProcessName == processName && p.Resource == resource)
+            var categorization = db.ActivityCategorizations.Where(p => (p.ConsumerId == consumerId || p.ConsumerId == null) && p.ProcessName == processName && p.Resource == resource)
                                     .FirstOrDefault();
-
-
 
             return categorization;
         }
