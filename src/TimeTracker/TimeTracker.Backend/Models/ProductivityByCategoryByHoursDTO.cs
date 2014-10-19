225ef9d1e866a1d7d22f0c0b9afa471e8f05a5c4
gogokaradjov <gogokaradjov@gmail.com>
2014-10-19 09:53:12 +0300

    Added two additional reports

---
+++ src/TimeTracker/TimeTracker.Backend/Models/ProductivityByCategoryByHoursDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracker.Backend.Models
+{
+    public class ProductivityByCategoryByHoursDTO
+    {
+        public string Label { get; set; }
+        public List<int> Values{ get; set; }
+    }
+}
