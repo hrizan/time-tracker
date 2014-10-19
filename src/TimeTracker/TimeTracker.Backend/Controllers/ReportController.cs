1b1a76207b05a257035aecfb8d4374987608ce23
gogokaradjov <gogokaradjov@gmail.com>
2014-10-19 12:19:11 +0300

    Demo data

--- src/TimeTracker/TimeTracker.Backend/Controllers/ReportController.cs
+++ src/TimeTracker/TimeTracker.Backend/Controllers/ReportController.cs
@@ -139,5 +139,235 @@ namespace TimeTracker.Backend.Controllers
 
             return result;
         }
+
+        //Demo
+        [AuthorizeToken]
+        public ProductivityPointsDTO GetProductivityPointsForDateDemo(DateTime forDate)
+        {
+            if (forDate == DateTime.Today)
+            {
+                return new ProductivityPointsDTO()
+                {
+                    Productive = 540,
+                    Distractive = 160,
+                };
+            }
+            else if (forDate == DateTime.Today.AddDays(-1))
+            {
+                return new ProductivityPointsDTO()
+                {
+                    Productive = 120,
+                    Distractive = 160,
+                };
+            }
+            else
+            {
+                return new ProductivityPointsDTO()
+                {
+                    Productive = 600,
+                    Distractive = 400,
+                };
+            }
+        }
+
+        [AuthorizeToken]
+        public ProductivityPointsByHours GetProductivityPointsForDateByHoursDemo(DateTime forDate)
+        {
+            if (forDate == DateTime.Today)
+            {
+                return new ProductivityPointsByHours()
+                {
+                    Productive = new List<int>() { 40, 2, 30, 120, 3, 4, 120, 213 },
+                    Distractive = new List<int>() { 12, 200, 30, 120, 323, 4, 12, 213 }
+                };
+            }
+            else if (forDate == DateTime.Today.AddDays(-1))
+            {
+                return new ProductivityPointsByHours()
+                {
+                    Productive = new List<int>() { 140, 123, 23, 1, 75, 60, 70, 100 },
+                    Distractive = new List<int>() { 120, 32, 432, 32, 3, 60, 50, 1 }
+                };
+            }
+            else
+            {
+                return new ProductivityPointsByHours()
+                {
+                    Productive = new List<int>() { 12, 233, 302, 23, 12, 43, 16, 23 },
+                    Distractive = new List<int>() { 40, 2, 30, 120, 3, 4, 120, 213 }
+                };
+            }
+        }
+
+        [AuthorizeToken]
+        public List<ProductivityByCategoryDTO> GetProductivityByCategoriesForDateDemo(DateTime forDate)
+        {
+            if (forDate == DateTime.Today)
+            {
+                return new List<ProductivityByCategoryDTO>()
+                {
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Software Development",
+                        Value = 600,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Social Networks",
+                        Value = 300,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Comunications",
+                        Value = 250,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Research",
+                        Value = 150,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Games",
+                        Value = 50,
+                    },
+                };
+            }
+            else if (forDate == DateTime.Today.AddDays(-1))
+            {
+               return new List<ProductivityByCategoryDTO>()
+                {
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Games",
+                        Value = 650,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Social Networks",
+                        Value = 400,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Research",
+                        Value = 400,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Comunications",
+                        Value = 300,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Software Development",
+                        Value = 78,
+                    },
+                };
+            }
+            else
+            {
+                return new List<ProductivityByCategoryDTO>()
+                {
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Research",
+                        Value = 1000,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Software Development",
+                        Value = 500,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Social Networks",
+                        Value = 100,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Games",
+                        Value = 0,
+                    },
+                    new ProductivityByCategoryDTO()
+                    {
+                        Label = "Comunications",
+                        Value = 0,
+                    },
+                    
+                };
+            }
+            throw new NotImplementedException();
+
+        }
+
+        [AuthorizeToken]
+        public List<ProductivityByCategoryByHoursDTO> GetProductivityByCategoriesForDateByHoursDemo(DateTime forDate)
+        {
+            if (forDate == DateTime.Today)
+            {
+                return new List<ProductivityByCategoryByHoursDTO>()
+                {
+                    new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Software Development",
+                        Values = new List<int>(){ 40, 30, 15, 25, 24},
+                    },
+                    new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Social Networks",
+                        Values = new List<int>(){ 10, 20, 20, 10, 15},
+                    },
+                     new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Communication",
+                        Values = new List<int>(){ 10, 10, 15, 25, 10},
+                    },
+                };
+            }
+            else if (forDate == DateTime.Today.AddDays(-1))
+            {
+                return new List<ProductivityByCategoryByHoursDTO>()
+                {
+                     new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Social Networks",
+                        Values = new List<int>(){ 10, 20, 20, 10, 15},
+                    },
+                    new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Software Development",
+                        Values = new List<int>(){ 40, 30, 15, 25, 24},
+                    },
+                     new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Communication",
+                        Values = new List<int>(){ 10, 10, 15, 25, 10},
+                    },
+                };
+            }
+            else
+            {
+                return new List<ProductivityByCategoryByHoursDTO>()
+                {
+                    new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Communication",
+                        Values = new List<int>(){ 40, 30, 15, 25, 24},
+                    },
+                    new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Social Networks",
+                        Values = new List<int>(){ 10, 20, 20, 10, 15},
+                    },
+                     new ProductivityByCategoryByHoursDTO()
+                    {
+                        Label = "Software Development",                        
+                        Values = new List<int>(){ 10, 10, 15, 25, 10},
+                    },
+                };
+            }
+            throw new NotImplementedException();
+
+        }
     }
 }
